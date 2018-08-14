ed887783b38efeacdcd6a66045d862a1200d5617
xuzehou <xuzehou@momenta.ai>
2018-08-14 14:10:25 +0800

    init some codes

---
+++ 74_Search_a_2D_Matrix.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        l = 0
+        r = len(matrix) - 1
+        while l <= r:
+            m = (l + r) / 2
+            if target >= matrix[m][0] and target <= matrix[m][-1]:
+                l = m
+                break
+            if target < matrix[m][0]:
+                r = m - 1
+            elif target > matrix[m][-1]:
+                l = m + 1
+        if l > r:
+            return False
+        row = l
+        l = 0
+        r = len(matrix[row]) - 1
+        while l <= r:
+            m = (l+r) / 2
+            if matrix[row][m] == target:
+                return True
+            if matrix[row][m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return False
+
